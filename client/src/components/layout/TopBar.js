53d3413fb002ac4043b1da0a64cf11907a05b98c
dericholas <nickderis@gmail.com>
2024-01-31 16:40:15 -0500

    First css- focused on nav-bar, and body

--- client/src/components/layout/TopBar.js
+++ client/src/components/layout/TopBar.js
@@ -4,8 +4,8 @@ import SignOutButton from "../authentication/SignOutButton";
 
 const TopBar = ({ user }) => {
   const unauthenticatedListItems = [
-    <li key="sign-in">
-      <Link to="/user-sessions/new">Sign In</Link>
+    <li key="sign-in" className="menu">
+      <Link to="/user-sessions/new" className="menu">Sign In</Link>
     </li>,
     <li key="sign-up">
       <Link to="/users/new" className="button">
@@ -21,18 +21,20 @@ const TopBar = ({ user }) => {
   ];
 
   return (
+    <div className="nav-bar">
     <div className="top-bar">
       <div className="top-bar-left">
         <ul className="menu">
           <li className="menu-text">App</li>
           <li>
-            <Link to="/">Home</Link>
+            <Link to="/" className="menu">Home</Link>
           </li>
         </ul>
       </div>
       <div className="top-bar-right">
         <ul className="menu">{user ? authenticatedListItems : unauthenticatedListItems}</ul>
       </div>
+    </div>
     </div>
   );
 };
