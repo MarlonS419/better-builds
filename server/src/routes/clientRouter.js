b131115ff179bffdb4bba85c788a4462e000839b
CornFrog <thecornfrog@gmail.com>
2024-02-06 10:03:07 -0500

    Removed missed testing route

--- server/src/routes/clientRouter.js
+++ server/src/routes/clientRouter.js
@@ -2,7 +2,7 @@ import express from "express";
 import getClientIndexPath from "../config/getClientIndexPath.js";
 
 const router = new express.Router();
-const clientRoutes = ["/", "/user-sessions/new", "/users/new", "/builds", "/builds/:id", "/builds/new", "/blah"];
+const clientRoutes = ["/", "/user-sessions/new", "/users/new", "/builds", "/builds/:id", "/builds/new"];
 const authedClientRoutes = ["/profile"];
 
 router.get(clientRoutes, (req, res) => {
